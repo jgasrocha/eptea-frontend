ae66b809a84d2233d480a2ab18ae05d847767057
João Gabriel <gabrieldeveloper33@gmail.com>
2026-08-26 23:47:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false, // Impede que o código original seja exposto no navegador
+  }
 })
